fe79f0203bc0cf400e7dbf153a4b60c4d2b8b8d1
Nicholas Lydon <nickkell@gmail.com>
2016-03-13 16:33:59 +0000

    added tslint and cleaned code up

--- app/app.tsx
+++ app/app.tsx
@@ -1,47 +1,45 @@
-import React = require('react');
+import * as React from 'react';
 import {Todo} from './todo.tsx';
 
-const App = React.createClass<any,{}>({
-	
-	getInitialState() { 
-		return {
-			todos: [],
-			newTodoName: '',
-			todoId: 0
-		};
-	},	
-	
-	render() {		
-		const handleNewTodo = (e) => {
-			if(/\S/.test(e.target.value)) {
-				this.setState({
-					todos: this.state.todos.concat([{
-						name: e.target.value,
-						finished: false,
-						removed: false,
-						key: this.state.todoId
-					}]),
-					todoId: this.state.todoId + 1,
-					newTodoName: ''
-				});
-			}
+const App = React.createClass<any, {}>({
+    getInitialState() {
+        return {
+            todos: [],
+            newTodoName: '',
+            todoId: 0
         };
-		
-		const handleNameChange = (e) => {
-			this.setState({
-				newTodoName: e.target.value
-			});						
+    },
+    render() {
+        const handleNewTodo = (e) => {
+            if (/\S/.test(e.target.value)) {
+                this.setState({
+                    todos: this.state.todos.concat([{
+                        name: e.target.value,
+                        finished: false,
+                        removed: false,
+                        key: this.state.todoId
+                    }]),
+                    todoId: this.state.todoId + 1,
+                        newTodoName: ''
+                    });
+            }
         };
-						
-		return (
-			<div>
-				<ul>{this.state.todos.map(function(todo) {
-					return <Todo todo={todo} />;			
-				})}</ul>
-				<input type="text" value={this.state.newTodoName} onChange={handleNameChange} onBlur={handleNewTodo} />
-			</div>	
-		);
-	}
+
+        const handleNameChange = (e) => {
+            this.setState({
+                newTodoName: e.target.value
+            });
+        };
+
+        return (
+            <div>
+                <ul>{this.state.todos.map(function(todo) {
+                    return <Todo todo={todo} />;
+                })}</ul>
+                <input type='text' value={this.state.newTodoName} onChange={handleNameChange} onBlur={handleNewTodo} />
+            </div>
+        );
+    }
 });
 
 export {App}
--- app/todo.tsx
+++ app/todo.tsx
@@ -1,52 +1,51 @@
 import React = require('react');
 
-const Todo = React.createClass<any,{}>({
-	
-	getInitialState() {
-		return {
-			editing: false,
-			newName: this.props.todo.name,
-			currentName: this.props.todo.name
-		};
-	},		
-	
-	render() {
-		const startEditing = () => {
-			this.setState({    
-				editing: true
-			});
-		};
-		
-		const updateName = (e) => {
-			this.setState({
-				newName: e.target.value
-			});
-		};
-		
-		const stopEditing = (e) => {
-			if(/\S/.test(e.target.value)) {
-				return this.setState({
-					editing:false,
-					newName: e.target.value,
-					currentName: e.target.value
-				});
-			}
-			
-			this.setState({
-				editing:false
-			});
-		};
-		
-		const getInput = () => {
-			return <input type="text" value={this.state.newName} onChange={updateName} onBlur={stopEditing} />;
-		};
-		
-		const getNameSpan = () => {
-			return <span onDoubleClick={startEditing}>{this.state.currentName}</span>;
+const Todo = React.createClass<any, {}>({
+
+    getInitialState() {
+        return {
+            editing: false,
+            newName: this.props.todo.name,
+            currentName: this.props.todo.name
         };
-		
-		return <li key={this.props.todo.key}>{this.state.editing ? getInput() : getNameSpan()}</li>;
-	}
+    },
+    render() {
+        const startEditing = () => {
+            this.setState({
+                editing: true
+            });
+        };
+
+        const updateName = (e) => {
+            this.setState({
+                newName: e.target.value
+            });
+        };
+
+        const stopEditing = (e) => {
+            if (/\S/.test(e.target.value)) {
+                return this.setState({
+                    editing: false,
+                    newName: e.target.value,
+                    currentName: e.target.value
+                });
+            }
+
+            this.setState({
+                editing: false
+            });
+        };
+
+        const getInput = () => {
+            return <input type='text' value={this.state.newName} onChange={updateName} onBlur={stopEditing} />;
+        };
+
+        const getNameSpan = () => {
+            return <span onDoubleClick={startEditing}>{this.state.currentName}</span>;
+        };
+
+        return <li key={this.props.todo.key}>{this.state.editing ? getInput() : getNameSpan()}</li>;
+    }
 });
 
 export {Todo}
